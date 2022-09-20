d566f46e418b192aadcc2f390e85aa2536a969db
Jon Hause <jonrhause@gmail.com>
2022-09-19 19:14:15 -0600

    commit tree 8ec7789930565c8e2dd7f6d8b2f9010c3bdaf766

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = `This one is on me!`;
+  } else if (distance >= 400 && distance <= 2000) {
+    result = `That will be twenty bucks.`;
+  } else if (distance > 2000 && distance <= 2500) {
+    result = `I will gladly take your thirty bucks.`;
+  } else if (distance > 2500) {
+    result = `No can do.`;
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+/*function ternaryCheckCity(city){
+  let output
+  if (city = 'NYC') {
+    output =  `Ok, sounds good.`;
+  } else if !(city = 'NYC') {
+    output = 'No go.'
+  }
+  return output
+}*/
+
+function ternaryCheckCity(city){
+  let output
+  city === 'NYC' ? (output = `Ok, sounds good.`) : (output = `No go.`);
+  return output
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let gratitude;
+  switch (tip) {
+    case `generous`:
+      gratitude = `Thank you so much.`;
+      break;
+    case `not as generous`:
+      gratitude =  `Thank you.`;
+      break;
+    default:
+      gratitude = `Bye.`;
+      break;
+  }
+  return gratitude;
 }
